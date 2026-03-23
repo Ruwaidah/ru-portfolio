eaf4cc030a26729e6e1b37207d98ec4c705d38fd
Ruwaidah Alfakhri <ruwaidah.riyadh@gmail.com>
2026-03-22 19:18:01 -0500

    styling footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -12,12 +12,12 @@ export default function Footer() {
 
         <div className="flex flex-wrap items-center gap-x-6 gap-y-2 text-sm text-mutetext">
           {/* Site links */}
-          <Link className="hover:text-text transition" to="/about">About</Link>
-          <Link className="hover:text-text transition" to="/projects">Projects</Link>
-          <Link className="hover:text-text transition" to="/contact">Contact</Link>
+          <Link className="md:inline-block hidden hover:text-text transition" to="/about">About</Link>
+          <Link className="md:inline-block hidden hover:text-text transition" to="/projects">Projects</Link>
+          <Link className="md:inline-block hidden hover:text-text transition" to="/contact">Contact</Link>
 
           {/* Divider */}
-          <span className="hidden md:inline-block h-4 w-px bg-white/10" />
+          <span className="hidden md:inline-block  h-4 w-px bg-white/10" />
 
           {/* Social links */}
           <a
